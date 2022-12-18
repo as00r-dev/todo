7ab8311c7dbf8a50b2d424fbd0317991ba398323
as00r <as00r.dev@gmail.com>
2022-12-18 18:21:33 +0530

    Fix import url

--- src/app/imports.js
+++ src/app/imports.js
@@ -1,5 +1,5 @@
 import { app } from "./todoElem";
-import { hi } from "/src/ui/components/hello-world/hi";
+import { hi } from "../ui/components/hello-world/hi";
 import { body } from "../ui/components/body/body";
 
 export { hi, body, app };
