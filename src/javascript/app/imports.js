aa359e07d1440e38150147f71f4123d63f7d29c4
as00r <as00r.dev@gmail.com>
2022-12-19 20:00:42 +0530

    Change plan
    Previous folder structure was confusing. Changed it.

--- src/javascript/app/imports.js
+++
@@ -1,4 +0,0 @@
-import { app } from "./todo-html-tag";
-import { body } from "../ui/dom-elems/body";
-
-export { body, app };
--- src/javascript/ui/dom-elems/body.js
+++
@@ -1,5 +0,0 @@
-import { app } from "../../app/todo-html-tag";
-
-export const body = document.querySelector("body");
-
-body.appendChild(app);
