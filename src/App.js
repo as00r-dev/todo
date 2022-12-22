0082d54ccc27f40ccb905bfda674355e7a388e73
as00r <as00r.dev@gmail.com>
2022-12-22 20:16:16 +0530

    Add footer, main tag in body

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import "./css/root.css";
 import "./css/utils.css";
 import { Topbar } from "./ui/components/topbar/topbar";
 import { Div } from "./ui/elem/div/div";
+import { Main } from "./ui/elem/main-html/main-html";
+import { Footer } from "./ui/elem/footer/footer";
 
 const root = Div();
-root.addClasses(["root"]);
-root.getElem.append(Topbar.getElem);
+const main = Main();
+const footer = Footer();
 
+root.addClasses(["root"]);
+
+Topbar.render(root.getElem, "afterbegin");
+main.render(Topbar.getElem, "afterend");
+footer.render(root.getElem, "beforeend");
 root.render(document.body, "afterbegin");
